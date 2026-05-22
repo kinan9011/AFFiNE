648efcc6d279709996f312d6a094aac88cf7c14e
Kinan <68794874+kinan9011@users.noreply.github.com>
2026-05-22 13:56:25 +0200

    Update sync-upstream.sh

--- docs/Spark/sync-upstream.sh
+++ docs/Spark/sync-upstream.sh
@@ -9,6 +9,6 @@
   git rebase canary
   echo "Spark is up to date with upstream canary"
 
-  chmod +x sync-upstream.sh
-  ./sync-upstream.sh  # run whenever upstream updates
-
+  # chmod +x sync-upstream.sh
+  # ./sync-upstream.sh  # run whenever upstream updates
+  # git push origin spark --force-with-lease
